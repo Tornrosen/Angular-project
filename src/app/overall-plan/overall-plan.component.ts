677432f3e22a432417089f8e83776a1c2140dfb9
Tornrosen <elec11anna@hotmail.com>
2025-05-16 16:21:00 +0200

    skapa utvecklingsmiljö och filstruktur

---
+++ src/app/overall-plan/overall-plan.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overall-plan',
+  imports: [],
+  templateUrl: './overall-plan.component.html',
+  styleUrl: './overall-plan.component.css'
+})
+export class OverallPlanComponent {
+
+}
